75a2c7a9ede2bd36ee242fcbae9ca1eabab7d3d0
claeo001 <88322812+claeo001@users.noreply.github.com>
2022-02-07 23:28:48 +0900

    AppConfig를 통해 관심사를 분리함.

--- src/test/java/Hello/core/member/MemberServiceTest.java
+++ src/test/java/Hello/core/member/MemberServiceTest.java
@@ -1,13 +1,20 @@
 package Hello.core.member;
 
 
+import Hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
-    MemberService memberService = new MemberServicelmpl();
+    MemberService memberService;
 
+    @BeforeEach
+    public void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
     @Test
     void join() {
         //given
