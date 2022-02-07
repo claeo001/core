cf41f51534733710f21e13f2447e159cad17db7d
claeo001 <88322812+claeo001@users.noreply.github.com>
2022-02-07 22:40:35 +0900

    OCP DIP 실패.

--- src/main/java/Hello/core/order/OrderServiceImpl.java
+++ src/main/java/Hello/core/order/OrderServiceImpl.java
@@ -9,8 +9,11 @@ import Hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();  // 고정 할인 금액 변경
+    private DiscountPolicy discountPolicy;  // 인터페이스 의존.?
+    // OCP DIP 같은 객체지향 설계원칙을 준수한 것 같지만 아니다.
 
+    @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
         int discountPrice = discountPolicy.discount(member, itemPrice);
---
+++ src/test/java/Hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,40 @@
+package Hello.core.discount;
+
+import Hello.core.member.Grade;
+import Hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP는 10% 할인이 적용되어야 한다.")
+    void vip_o(){
+        //given
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+        //then
+        assertThat(discount).isEqualTo(1000);   // Assertions 변경. static import 찾아보기.
+    }
+    // 성공 테스트도 좋은데 실패 테스트도 만들어 봐야함 .
+    @Test
+    @DisplayName("VIP가 아니면 할인이 적용되지 않아야 한다")
+    void vip_x(){
+        //given
+        Member member = new Member(2L, "memberBASIC", Grade.BASIC);
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+        //then
+        assertThat(discount).isEqualTo(0);   // 값을 변경.
+
+
+    }
+
+}
