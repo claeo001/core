45e498a2d009ea7320797f58ca98eca5c6713079
claeo001 <88322812+claeo001@users.noreply.github.com>
2022-02-08 10:57:56 +0900

    Spring 전환 전.

--- src/main/java/Hello/core/AppConfig.java
+++ src/main/java/Hello/core/AppConfig.java
@@ -1,6 +1,9 @@
 package Hello.core;
 
+import Hello.core.discount.DiscountPolicy;
 import Hello.core.discount.FixDiscountPolicy;
+import Hello.core.discount.RateDiscountPolicy;
+import Hello.core.member.MemberRepository;
 import Hello.core.member.MemberService;
 import Hello.core.member.MemberServicelmpl;
 import Hello.core.member.MemoryMemberRepository;
@@ -10,17 +13,25 @@ import Hello.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService(){
-        return new MemberServicelmpl(new MemoryMemberRepository());
+        return new MemberServicelmpl(memberRepository());
         // 생성자 주입
     }
 
-    public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
+    public OrderService orderService(){
+        return new OrderServiceImpl(memberRepository(), new FixDiscountPolicy());
+    }
 
+    public DiscountPolicy discountPolicy(){
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }   //FixDiscountPolicy  > RateDiscountPolicy
 }
 // 구현 객체를 생성.
 // 생성자를 통해서 주입 연결.
 // 객체의 생성과 연결을 담당하게 됨.
 // 직접적으로 받는게 아니다
+// 역할을 세우고 구현을 만드는게 좋다.
